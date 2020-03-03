69fc90b4d1705143a146f1a2e4a15d6fd8220db1
Vova <vova77gh@gmail.com>
2020-03-03 10:34:13 +0200

    Created private class API service

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,28 +3,63 @@ import React from 'react';
 import './app.css';
 
 const App = () => {
-     const getResource = async (url) => {
-        const res = await fetch(url);
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url} resived status ${res.status}`)
+    class SwapiService { //клас-сервіс;
+                        // асинх ізольоване джерело даних
+        _apiBase = 'https://swapi.co/api'; //private часть класу
+
+
+        async getResourse(url) {
+            const res = await fetch(`${this._apiBase}${url}`);
+
+            if (!res.ok) {
+                throw new Error(`Could not fetch ${url} resived status ${res.status}`)
+            }
+
+            return await res.json();
+        };
+
+        async getAllPeople() {
+            const res = await this.getResourse('/people/')
+            return res.results;
         }
 
-        const body = await res.json();
-        return body
-     };
+        getPerson(id) {
+            return this.getResourse(`/people/${id}/`)
+        }
 
-     getResource('https://swapi.co/api/people/1/')
-         .then((body) => console.log(body))
-         .catch((err) => console.log('Could not fetch', err));
+        async getAllPlanets() {
+            const res = await this.getResourse('/planets/')
+            return res.results;
+        }
 
+        getPlanet(id) {
+            return this.getResourse(`/planets/${id}/`)
+        }
 
+        async getAllStarships() {
+            const res = await this.getResourse('/starships/')
+            return res.results;
+        }
+
+        getStarship(id) {
+            return this.getResourse(`/starships/${id}/`)
+        }
+
+    }
+
+    const swapi = new SwapiService();
+
+    swapi.getPerson(3)
+        .then(people => console.log(p.name))
+        .catch((err) => console.log('Could not fetch', err));
 
     return (
         <div>
 
         </div>
     )
+
 };
 
 export default App;
