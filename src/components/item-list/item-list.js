d77ba4d25f998293cdcba35f6fc21b2e007fc84f
Vova <vova77gh@gmail.com>
2020-03-08 04:32:34 +0200

    Loading peoples from swapi APi, make loader and error-indicator (item-list component)

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,21 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import SwapiService from '../../services/swapi-service';
 
 import './item-list.css';
+import Loader from '../loader/loader';
+import ErrorIndicator from '../error-indicator/error-indicator';
 
-const ItemList = () => {
-    return (
-        <ul className="list-group-flush list-group">
-            <li className="list-group-item l-item">
-                Luke Skywalker
-            </li>
-            <li className="list-group-item l-item">
-                Darth Vader
-            </li>
-            <li className="list-group-item l-item">
-                R2-D2
-            </li>
-        </ul>
-    );
-};
+export default class ItemList extends Component {
+    swapiService = new SwapiService();
 
-export default ItemList;
+    state = {
+        peopleList: [],
+        loading: true,
+        error: false
+    };
+
+    componentDidMount() {
+        this.loadPeoples();
+    }
+
+    onPeoplesLoaded = (peopleList) => {
+        this.setState({
+            peopleList,
+            loading: false
+        });
+    };
+
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false
+        });
+    };
+
+    loadPeoples = () => {
+        this.swapiService
+            .getAllPeople()
+            .then(this.onPeoplesLoaded)
+            .catch(this.onError);
+    };
+
+    renderPeoples = (persons) => {
+        return persons.map(({id, name}) => {
+            return (
+                <li className="list-group-item l-item"
+                    key={id}>
+
+                    {name}
+                </li>
+            );
+        });
+    };
+
+    render() {
+        const { peopleList, loading, error } = this.state;
+
+        const items = this.renderPeoples(peopleList);
+
+        const loader = loading ? <Loader /> : null;
+        const problem = error ? <ErrorIndicator /> : null;
+
+        return (
+            <ul className="list-group-flush list-group l-group">
+                {loader}
+                {problem}
+                {items}
+            </ul>
+        );
+    }
+}
